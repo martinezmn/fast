cc106cf0e4441c69ea8dea0b6a81e86da9f21ceb
Gabriel Martinez <gabrielmn378@gmail.com>
2021-05-26 10:05:37 -0300

    coment id integer

--- src/schemas/comments.schema.js
+++ src/schemas/comments.schema.js
@@ -72,7 +72,7 @@ module.exports = class CommentsSchema {
             params: {
                 type: 'object',
                 properties: {
-                    comment_id: { type: 'string' }
+                    comment_id: { type: 'integer' }
                 },
                 required: ['comment_id']
             },
